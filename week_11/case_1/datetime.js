f85c3078116e34c19dc6c39dd944612ed6814715
Nathan Pham <nathanpham.me@gmail.com>
2022-04-15 19:31:45 -0700

    week_11 > case_1 sky thingy

--- week_11/case_1/datetime.js
+++ week_11/case_1/datetime.js
@@ -1,37 +1,31 @@
-/*
-   New Perspectives on HTML and CSS
-   Tutorial 10
-   Case Problem 1
+// utilities
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => [...document.querySelectorAll(selector)];
 
-   Function List:
-   showDate
-      Used to return a text string containing the current date and time.
-   getMap
-      Used to the determine the current sky map number to display with the online planisphere
+// get the formatted date
+const getDateTime = () => {
+    const time = new Date();
+    return time.toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    });
+};
 
-*/
+// get map associated with time
+const getMap = () => {
+    const date = new Date();
+    return (date.getMonth() * 2 + date.getHours()) % 24;
+};
 
+const updateMap = () => {
+    $("#sky").src = `assets/sky${getMap()}.jpg`;
+    $("#datetime").textContent = getDateTime();
+};
 
-function showDateTime() {
-   var thisDate = new Date();
-   var thisWDay=thisDate.getDay();
-   var thisDay=thisDate.getDate();
-   var thisMonth=thisDate.getMonth();
-   var thisYear=thisDate.getFullYear();
-   var mName = new Array("January", "February", "March", "April", "May", 
-       "June", "July", "August", "September", "October","November", "December");
-   var hours=thisDate.getHours();
-   var minutes=thisDate.getMinutes();
-   ampm = hours >=12 ? " pm" : " am";
-   hours = hours > 12 ? hours-12 : hours;
-   minutes = minutes < 10 ? "0"+minutes : minutes;
-   return mName[thisMonth]+" "+thisDay+", "+thisYear + ", " + hours + ":" + minutes + ampm;
-}
-
-function getMap() {
-   thisTime = new Date();
-   hour = thisTime.getHours();
-   month = thisTime.getMonth();
-   mapNumber = (month*2+hour)%24;
-   return mapNumber;
-}
+// update map every second
+setInterval(updateMap, 1000);
+updateMap();
